b565e867249facb9a61c36182f71f8e88e7cf27d
Lee-Duckwon <a09519@naver.com>
2021-12-29 23:22:24 +0900

    Creaet: basic

---
+++ 1_basic/1_number.py
@@ -0,0 +1,12 @@
+print(-1) 
+print(0)
+print(1) # int  정수
+print(1.1) # float 실수
+print('2곱하기100은', 2*100)
+
+import math
+print('math.sqrt(4):', math.sqrt(4))
+print('math.pow(4):', math.pow(4,2))
+
+import random
+print('행운의 숫자', random.random())
---
+++ 1_basic/3_list.py
@@ -0,0 +1,12 @@
+students = ["sam", "owen", "edgar"]
+grades = [2, 1, 3]
+print(students[grades[1]])
+print(len(students)) #원소 수
+print(min(grades)) #가장 작은 값 
+print(max(grades)) 
+
+import statistics # 통계 임포트
+print(statistics.mean(grades)) # 평균 구하기
+
+import random
+print(random.choice(students)) # 랜덤 뽑기
